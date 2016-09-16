b4fa69e4eefddc4ff380df2040d555d1c432f186
John Mc <johnmcwilliams8@hotmail.com>
2016-09-16 14:47:15 +0100

    will start work on excel_bank sub-class

---
+++ f3m_accounts/excel_bank.rb
@@ -0,0 +1,30 @@
+require_relative 'excel'
+require 'awesome_print'
+
+class ExcelBank < Excel
+  def initialize(file_path)
+    import_file_contents file_path
+
+    row_hashes = []
+    @file_contents.sheets[0].rows.each do |row|
+      row_hash = get_row_hash row
+      row_hashes << row_hash
+    end
+
+    awesome_print row_hashes
+
+    # row = @file_contents.sheets[0].rows[0]
+    # awesome_print get_row_hash row
+  end
+
+  def get_row_hash(row)
+    {
+        account_code: row[0],
+        date:         row[1],
+        bank_info:    row[2],
+        amount:       row[3],
+        sub_code:     row[4],
+        memo:         row[5]
+    }
+  end
+end
